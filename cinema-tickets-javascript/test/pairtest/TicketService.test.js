61f96d3f818a5e39505a8f7587b64c7247be4a59
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2024-10-21 17:55:21 +0100

    move test in the designated place

--- cinema-tickets-javascript/test/pairtest/TicketService.test.js
+++ cinema-tickets-javascript/test/pairtest/TicketService.test.js
@@ -1,11 +1,29 @@
-const assert = require('assert');
+import TicketService from "../../src/pairtest/TicketService.js";
+import TicketTypeRequest from "../../src/pairtest/lib/TicketTypeRequest.js";
+import InvalidPurchaseException from "../../src/pairtest/lib/InvalidPurchaseException.js";
 
-test('Test case 1', () => {
-  // Write your test case here
+describe("TicketService", () => {
+  let ticketService;
+
+  beforeEach(() => {
+    ticketService = new TicketService();
+  });
+
+  it("should throw InvalidPurchaseException if accountId is not provided", () => {
+    expect(() => {
+      ticketService.purchaseTickets();
+    }).toThrow(InvalidPurchaseException);
+  });
+
+  it("should throw InvalidPurchaseException if ticketTypeRequests are not provided", () => {
+    expect(() => {
+      ticketService.purchaseTickets(1);
+    }).toThrow(InvalidPurchaseException);
+  });
+
+  it("should throw InvalidPurchaseException if ticketTypeRequests are invalid", () => {
+    expect(() => {
+      ticketService.purchaseTickets(1, {});
+    }).toThrow(InvalidPurchaseException);
+  });
 });
-
-test('Test case 2', () => {
-  // Write your test case here
-});
-
-// Add more test cases as needed
