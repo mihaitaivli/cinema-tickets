a839bd30087b8b9e053658e59d826884dc85bccb
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2024-10-21 17:38:58 +0100

    add initial base case tests

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,5 +1,5 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from "./lib/TicketTypeRequest.js";
+import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 
 export default class TicketService {
   /**
@@ -8,5 +8,24 @@ export default class TicketService {
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
+
+    // basic checks
+    if (!accountId) {
+      throw new InvalidPurchaseException("An accountId is required");
+    }
+
+    if (!ticketTypeRequests.length) {
+      throw new InvalidPurchaseException(
+        "The array of ticketTypeRequests cannot be empty"
+      );
+    }
+
+    for (const ticketTypeRequest of ticketTypeRequests) {
+      if (!(ticketTypeRequest instanceof TicketTypeRequest)) {
+        throw new InvalidPurchaseException(
+          "ticketTypeRequests must be an instance of TicketTypeRequest"
+        );
+      }
+    }
   }
 }
---
+++ cinema-tickets-javascript/test/pairtest/TicketService.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+test('Test case 1', () => {
+  // Write your test case here
+});
+
+test('Test case 2', () => {
+  // Write your test case here
+});
+
+// Add more test cases as needed
